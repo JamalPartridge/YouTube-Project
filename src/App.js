98e5fec8df52a221bbada1220087a6b39f72303d
JamalPartridge <jamalpartridge@pursuit.org>
2022-11-16 12:59:50 -0500

    added some routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+import Home from "./components/Home"
+import Video from "./components/Video"
+import CommentForm from "./components/CommentForm"
 
 function App() {
 
-  const[] = useState()
+  const[searchResults, setSearchResults] = useState([])
 
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/videos" element={<></>} />
+          <Route path="/videos/:id" element={<Video />} />
+          <Route path="/" element={<CommentForm />}/>
+        </Routes>
       </header>
     </div>
   );
