96ee3f33709e7b8db4a4e282cda9af4373f567ca
ronJohnPursuit <ronjohnson@pursuit.org>
2022-11-16 16:27:06 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from "./components/Home"
-import Video from "./components/Video"
-import CommentForm from "./components/CommentForm"
+import Home from "./components/Home";
+import Video from "./components/Video";
+import CommentForm from "./components/CommentForm";
 
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
 
-  const[searchResults, setSearchResults] = useState([])
+  function handleSearchResult() {
+    setSearchResults(/*api result [] */);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/videos" element={<></>} />
-          <Route path="/videos/:id" element={<Video />} />
-          <Route path="/" element={<CommentForm />}/>
-        </Routes>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/videos" element={<></>} />
+            <Route path="/videos/:id" element={<Video />} />
+            <Route path="/" element={<CommentForm />} />{" "}
+            {/* why comment form on landing page? */}
+          </Routes>
+        </Router>
       </header>
     </div>
   );
