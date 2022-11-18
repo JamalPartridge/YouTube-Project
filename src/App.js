fbf8100a38b8500408cfd1fa1407efc33e135d00
ronJohnPursuit <ronjohnson@pursuit.org>
2022-11-18 03:04:36 -0500

    added more to landing and got search bar working

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Video from "./components/Video";
 import CommentForm from "./components/CommentForm";
+import { snowboard } from "./mockdata";
 
 import "./App.css";
+
 function App() {
   const [searchResults, setSearchResults] = useState([]);
+
+  // thank you JD :)
+  console.log(snowboard.map((e) => e.snippet.title));
 
   function handleSearchResult() {
     setSearchResults(/*api result [] */);
   }
-  // thank you JD :)
+
   return (
     <div className="App">
       <header className="App-header">
@@ -25,7 +30,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/videos" element={<></>} />
           <Route path="/videos/:id" element={<Video />} />
-          <Route path="/" element={<CommentForm />} />{" "}
+          <Route path="/" element={<CommentForm />} />
+          {"  "}
           {/* why comment form on landing page? Should be apart of the /videos/:id path no? */}
         </Routes>
       </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,67 @@
+import React, { useState, useRef, useEffect } from "react";
+import { snowboard } from "../mockdata";
+// snowboard.map((e) => e.snippet.title)
+
+console.log(snowboard[1].snippet.title);
+
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [myNodelist, setNodeList] = useState(0);
+
+  useEffect(() => {
+    setNodeList(document.querySelectorAll("li").length);
+  }, [search]);
+  useEffect(() => {
+    setNodeList(document.querySelectorAll("li").length);
+  }, []);
+
+  const searchVal = useRef(null);
+
+  function handleSearch() {
+    setSearch(searchVal.current.value);
+  }
+  //   console.log(document.querySelectorAll("li").length);
+  console.log(myNodelist);
+  console.log(search);
   return (
     <div>
-      <h3>Search Bar and Search Button </h3>
+      <h3>
+        <label htmlFor="amount">
+          <input
+            ref={searchVal}
+            id="search"
+            name="search"
+            type="text"
+            placeholder="Search"
+          />
+        </label>{" "}
+        <button onClick={handleSearch}>Search</button>
+      </h3>
+      {(myNodelist && <h2> Search results for: {search} </h2>) ||
+        (search && <h2>No search results for: {search}</h2>) || (
+          <h2>Please search for a video 🎥</h2>
+        )}
+      <div className="video-cards">
+        {search &&
+          snowboard.map(
+            (e) =>
+              e.id.kind == "youtube#video" &&
+              (e.snippet.title.includes(search) ||
+                e.snippet.description.includes(search)) && (
+                <li className="list-video">
+                  <img
+                    className="img"
+                    alt="Search Term"
+                    src={`${e.snippet.thumbnails.default.url}`}
+                  ></img>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: e.snippet.title }}
+                  ></div>
+                  {/* {e.snippet.title} */}
+                </li>
+              )
+          )}
+      </div>
     </div>
   );
 }
