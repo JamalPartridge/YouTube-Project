8d1beeb7c89883879ed5c15d7c3fff0cb3ba3096
JamalPartridge <jamalpartridge@pursuit.org>
2022-11-18 13:47:31 -0500

    added modal

---
+++ src/components/ErrorMessage.js
@@ -0,0 +1,8 @@
+export default function ErrorMessage() {
+    return (
+      <section className="error">
+        <p>Opps! Something went wrong! Try again later.</p>
+      </section>
+    );
+  }
+  
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate} from "react-router-dom"
+
+// import { getVideoList } from 
 export default function Video({}) {
   return <div>Single video card</div>;
 }
